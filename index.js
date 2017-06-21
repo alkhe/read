90fd91b6e78284c68d0770d0736c72441e5320d2
edge <functaire@gmail.com>
2017-06-21 14:17:52 -0700

    add font selection

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const content = document.getElementById('content')
+const font = document.getElementById('font')
 const size = document.getElementById('size')
 
+function readjust_content() {
+	content.style.height = content.scrollHeight
+}
+
 document.addEventListener('paste', e => {
-	const text = e.clipboardData.getData('text').replace(/\n+/g, '<br /><br />')
-	content.innerHTML = text
+	const text = e.clipboardData.getData('text').replace(/\n+/g, '\n\n')
+	content.value = text
+	readjust_content()
 	e.stopPropagation()
 	e.preventDefault()
 })
 
 size.addEventListener('input', e => {
 	content.style.fontSize = e.target.value + 'px'
+	content.style.height = content.scrollHeight
+	readjust_content()
+})
+
+font.addEventListener('input', e => {
+	const choice = e.target.value
+	content.style.fontFamily = choice
+	readjust_content()
 })
