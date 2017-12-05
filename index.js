0a6c794ed92fecce6bca50fa9e1bb2cab19200af
edge <functaire@gmail.com>
2017-12-04 16:33:21 -0800

    add whitney, improve font look

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ const font = document.getElementById('font')
 const size = document.getElementById('size')
 
 function readjust_content() {
-	content.style.height = content.scrollHeight
+  content.style.height = content.scrollHeight
 }
 
 document.addEventListener('paste', e => {
-	const text = e.clipboardData.getData('text').replace(/\n+/g, '\n\n')
-	content.value = text
-	readjust_content()
-	e.stopPropagation()
-	e.preventDefault()
+  const text = e.clipboardData.getData('text').replace(/\n+/g, '\n\n')
+  content.value = text
+  readjust_content()
+  e.stopPropagation()
+  e.preventDefault()
 })
 
 size.addEventListener('input', e => {
-	content.style.fontSize = e.target.value + 'px'
-	content.style.height = content.scrollHeight
-	readjust_content()
+  content.style.fontSize = e.target.value + 'px'
+  content.style.height = content.scrollHeight
+  readjust_content()
 })
 
 font.addEventListener('input', e => {
-	const choice = e.target.value
-	content.style.fontFamily = choice
-	readjust_content()
+  const choice = e.target.value
+  content.style.fontFamily = choice
+  readjust_content()
 })
+
